f7f5505128006bce96003767d60fd1cc4e26175b
shawn.rusaw <shawn.rusaw@d05bc82c-ee18-d417-4187-6ce51f18356a>
2011-12-15 16:35:42 +0000

    - changed terminology from old/new to source/target

--- applications/blobstore_migration/blobstore_migration_fsm.py
+++ applications/blobstore_migration/blobstore_migration_fsm.py
@@ -1,25 +1,25 @@
 """ blobstore migration 
 
-This system consists of 2 state machines, one that runs on the old (Master-Slave) system
-and one that run on the new (HRD) system.
+This system consists of 2 state machines, one that runs on the source (Master-Slave) system
+and one that run on the target (HRD) system.
 
-"SendBlobKeysToNewApp" runs on the old system and is responsible for:
+"SendBlobKeysToTargetApp" runs on the source system and is responsible for:
 
 1. continuing over blobstore.BlobInfo entities
-2. starting up a new "PullBlobDataFromOldApp" machine instance on the new system
+2. starting up a target "PullBlobDataFromSourceApp" machine instance on the target system
 
-"PullBlobDataFromOldApp" runs on the new system and is responsible for:
+"PullBlobDataFromSourceApp" runs on the target system and is responsible for:
 
-1. calling http://old//blobinfo_migration/pull_endpoint/ to fetch old blob data
-2. saving the blob data into a new blob
-3. storing a mapping of oldBlobKey->newBlobKey in a BlobKeyMap instance
+1. calling http://source//blobinfo_migration/pull_endpoint/ to fetch source blob data
+2. saving the blob data into a target blob
+3. storing a mapping of sourceBlobKey->targetBlobKey in a BlobKeyMap instance
 
-Idempotency is achieved by keying BlobKeyMap instance off the oldBlobKeys, so even if multiple
-copies are pulled across into the new system, in the end there is a single mapping of old->new.
-This has the effect of favoring orphaned blobs on the new system over missing blobs in the new
+Idempotency is achieved by keying BlobKeyMap instance off the sourceBlobKeys, so even if multiple
+copies are pulled across into the target system, in the end there is a single mapping of source->target.
+This has the effect of favoring orphaned blobs on the target system over missing blobs in the target
 system.
 
-TODO: User code to resolve old blobs (via old email links) to the new blobs.
+TODO: User code to resolve source blobs (via source email links) to the target blobs.
 """
 import urllib
 
@@ -35,14 +35,14 @@ from google.appengine.ext import db
 DEFAULT_TASK_NAME = 'blobstore-migration'
 
 TASK_NAME_PARAM = 'task_name'
-OLD_HOST_PARAM = 'old_host'
-NEW_HOST_PARAM = 'new_host'
-OLD_BLOB_KEY_PARAM = 'old_blob_key'
-OLD_BLOB_FILENAME_PARAM = 'old_blob_filename'
-OLD_BLOB_CONTENT_TYPE_PARAM = 'old_blob_content_type'
+SOURCE_HOST_PARAM = 'source_host'
+TARGET_HOST_PARAM = 'target_host'
+SOURCE_BLOB_KEY_PARAM = 'source_blob_key'
+SOURCE_BLOB_FILENAME_PARAM = 'source_blob_filename'
+SOURCE_BLOB_CONTENT_TYPE_PARAM = 'source_blob_content_type'
 
-SEND_MACHINE_NAME = 'SendBlobKeysToNewApp'
-PULL_MACHINE_NAME = 'PullBlobDataFromOldApp'
+SEND_MACHINE_NAME = 'SendBlobKeysToTargetApp'
+PULL_MACHINE_NAME = 'PullBlobDataFromSourceApp'
 
 # changing BASE_PATH is allowed, but be sure to also change app.yaml
 BASE_PATH = 'blobstore_migration'
@@ -51,91 +51,91 @@ PULL_ENDPOINT_URL = 'http://%s/' + BASE_PATH + '/pull_endpoint/'
 
 class BlobKeyMap( db.Model ):
     """ Maps Master/Slave blobs to High-Replication blobs """
-    oldBlobKey = db.StringProperty(required=True, indexed=False) # already indexed in key_name
-    newBlobKey = db.StringProperty(required=True)
+    sourceBlobKey = db.StringProperty(required=True, indexed=False) # already indexed in key_name
+    targetBlobKey = db.StringProperty(required=True)
     
 class Send( DatastoreContinuationFSMAction ):
-    """ Sends info about old blobs to the new HRD application """
+    """ Sends info about source blobs to the target HRD application """
     
     def getQuery(self, context, obj):
         """ For all blobs """
         return blobstore.BlobInfo.all()
     
     def execute(self, context, obj):
-        """ Simply POSTs info about the old blob to the new application """
+        """ Simply POSTs info about the source blob to the target application """
         
         if obj.get(CONTINUATION_RESULT_KEY):
             
-            # get the information about the hosts and contruct a url to trigger pull on new app
-            oldHost = context[OLD_HOST_PARAM]
-            newHost = context[NEW_HOST_PARAM]
-            startPullUrl = START_PULL_URL % newHost
-            oldBlobInfo = obj[CONTINUATION_RESULT_KEY]
+            # get the information about the hosts and contruct a url to trigger pull on target app
+            sourceHost = context[SOURCE_HOST_PARAM]
+            targetHost = context[TARGET_HOST_PARAM]
+            startPullUrl = START_PULL_URL % targetHost
+            sourceBlobInfo = obj[CONTINUATION_RESULT_KEY]
             taskName = context.get(TASK_NAME_PARAM, DEFAULT_TASK_NAME)
             
-            # trigger a pull state machine on the new app
+            # trigger a pull state machine on the target app
             response = urlfetch.fetch(startPullUrl, 
-                                      payload=urllib.urlencode({OLD_BLOB_KEY_PARAM: str(oldBlobInfo.key()),
-                                                                OLD_BLOB_FILENAME_PARAM: oldBlobInfo.filename,
-                                                                OLD_BLOB_CONTENT_TYPE_PARAM: oldBlobInfo.content_type,
-                                                                OLD_HOST_PARAM: oldHost,
-                                                                NEW_HOST_PARAM: newHost,
+                                      payload=urllib.urlencode({SOURCE_BLOB_KEY_PARAM: str(sourceBlobInfo.key()),
+                                                                SOURCE_BLOB_FILENAME_PARAM: sourceBlobInfo.filename,
+                                                                SOURCE_BLOB_CONTENT_TYPE_PARAM: sourceBlobInfo.content_type,
+                                                                SOURCE_HOST_PARAM: sourceHost,
+                                                                TARGET_HOST_PARAM: targetHost,
                                                                 TASK_NAME_PARAM: taskName}),
                                       method=urlfetch.POST,
                                       headers={'Content-Type': 'application/x-www-form-urlencoded'})
             assert response.status_code == 200
             
     @staticmethod
-    def getOldBlobData(oldBlobKey):
+    def getSourceBlobData(sourceBlobKey):
         """ Sends blob data """
-        oldBlobInfo = blobstore.BlobInfo.get(oldBlobKey)
-        blobReader = blobstore.BlobReader(oldBlobInfo)
+        sourceBlobInfo = blobstore.BlobInfo.get(sourceBlobKey)
+        blobReader = blobstore.BlobReader(sourceBlobInfo)
         data = blobReader.read() # raises BlobNotFoundError if missing
         return data
     
 class Pull( FSMAction ):
-    """ Pulls info about old blobs into the new HRD application """
+    """ Pulls info about source blobs into the target HRD application """
     
     def execute(self, context, obj):
-        """ Fetch the old blob data and writes it to the new application """
+        """ Fetch the source blob data and writes it to the target application """
         
         # fetch the details about the blob
-        oldBlobKey = context[OLD_BLOB_KEY_PARAM]
-        blobKeyMap = BlobKeyMap.get_by_key_name(oldBlobKey)
+        sourceBlobKey = context[SOURCE_BLOB_KEY_PARAM]
+        blobKeyMap = BlobKeyMap.get_by_key_name(sourceBlobKey)
         
         if not blobKeyMap:
             
-            oldBlobContentType = context[OLD_BLOB_CONTENT_TYPE_PARAM]
-            oldBlobFilename = context[OLD_BLOB_FILENAME_PARAM]
+            sourceBlobContentType = context[SOURCE_BLOB_CONTENT_TYPE_PARAM]
+            sourceBlobFilename = context[SOURCE_BLOB_FILENAME_PARAM]
             
-            # construct the url in the old app to fetch the blob data
-            oldHost = context[OLD_HOST_PARAM]
-            pullEndpointUrl = PULL_ENDPOINT_URL % oldHost + '?' + OLD_BLOB_KEY_PARAM + '=' + oldBlobKey
+            # construct the url in the source app to fetch the blob data
+            sourceHost = context[SOURCE_HOST_PARAM]
+            pullEndpointUrl = PULL_ENDPOINT_URL % sourceHost + '?' + SOURCE_BLOB_KEY_PARAM + '=' + sourceBlobKey
             
             # fetch the data and assert that the response is valid
             response = urlfetch.fetch(pullEndpointUrl, method=urlfetch.GET)
             assert response.status_code == 200
             
-            # now write the blob data back down into the new application
-            fileName = files.blobstore.create(mime_type=oldBlobContentType, 
-                                              _blobinfo_uploaded_filename=oldBlobFilename)
+            # now write the blob data back down into the target application
+            fileName = files.blobstore.create(mime_type=sourceBlobContentType, 
+                                              _blobinfo_uploaded_filename=sourceBlobFilename)
             f = files.open(fileName, 'a')
             f.write(response.content)
             f.close()
             files.finalize(fileName)
             
-            # create a mapping between the old blob and the new blob
-            newBlobKey = files.blobstore.get_blob_key(fileName)
-            blobKeyMap = BlobKeyMap(key_name=oldBlobKey, oldBlobKey=oldBlobKey, newBlobKey=str(newBlobKey))
+            # create a mapping between the source blob and the target blob
+            targetBlobKey = files.blobstore.get_blob_key(fileName)
+            blobKeyMap = BlobKeyMap(key_name=sourceBlobKey, sourceBlobKey=sourceBlobKey, targetBlobKey=str(targetBlobKey))
             blobKeyMap.put()
             
         # perform some optional user data-model migration
-        self.migrate(blobKeyMap.oldBlobKey, blobKeyMap.newBlobKey)
+        self.migrate(blobKeyMap.sourceBlobKey, blobKeyMap.targetBlobKey)
         
-    def migrate(self, oldBlobKey, newBlobKey):
+    def migrate(self, sourceBlobKey, targetBlobKey):
         """ Contains user code to migrate data 
         
-        @param oldBlobKey: a string blob key referenceing the old application
-        @param newBlobKey: a string blob key referenceing the new application
+        @param sourceBlobKey: a string blob key referenceing the source application
+        @param targetBlobKey: a string blob key referenceing the target application
         """
         pass
--- applications/blobstore_migration/main.py
+++ applications/blobstore_migration/main.py
@@ -9,9 +9,9 @@ from google.appengine.api.taskqueue import TaskAlreadyExistsError
 from google.appengine.api.taskqueue import TombstonedTaskError
 
 from blobstore_migration_fsm import Send
-from blobstore_migration_fsm import OLD_BLOB_KEY_PARAM
-from blobstore_migration_fsm import OLD_HOST_PARAM
-from blobstore_migration_fsm import NEW_HOST_PARAM
+from blobstore_migration_fsm import SOURCE_BLOB_KEY_PARAM
+from blobstore_migration_fsm import SOURCE_HOST_PARAM
+from blobstore_migration_fsm import TARGET_HOST_PARAM
 from blobstore_migration_fsm import TASK_NAME_PARAM
 from blobstore_migration_fsm import DEFAULT_TASK_NAME
 from blobstore_migration_fsm import SEND_MACHINE_NAME
@@ -23,10 +23,10 @@ class PullEndpointHandler(webapp.RequestHandler):
     """ Listening on the Master-Slave application """
     def get(self):
         """ see RequestHandler.get() """
-        oldBlobKey = self.request.GET[OLD_BLOB_KEY_PARAM]
+        sourceBlobKey = self.request.GET[SOURCE_BLOB_KEY_PARAM]
         try:
-            oldBlobData = Send.getOldBlobData(oldBlobKey)
-            self.response.out.write(oldBlobData)
+            sourceBlobData = Send.getSourceBlobData(sourceBlobKey)
+            self.response.out.write(sourceBlobData)
         except blobstore.BlobNotFoundError:
             self.error(404)
             
@@ -37,8 +37,8 @@ CONTENT = """
 </head>
 <body>
 <form method="POST">
-Source: http://<input name="old_host" value="%(old_host)s" size="50"/>%(path)s<br/>
-Destination: http://<input name="new_host" value="%(new_host)s" size="50"/>%(path)s<br/>
+Source: http://<input name="source_host" value="%(source_host)s" size="50"/>%(path)s<br/>
+Destination: http://<input name="target_host" value="%(target_host)s" size="50"/>%(path)s<br/>
 Task Name: <input name="task_name" value="%(task_name)s" size="25"/><br/>
 <input type="submit"/>
 </form>
@@ -55,18 +55,18 @@ class StartMigrationHandler(webapp.RequestHandler):
         """ see RequestHandler.get() """
         app = os.environ['APPLICATION_ID']
         version = os.environ['CURRENT_VERSION_ID'].split('.')[0]
-        oldHost = '%s.%s.appspot.com' % (version, app)
-        newHost = '%s.FIXME.appspot.com' % (version,)
-        content = CONTENT % {OLD_HOST_PARAM: oldHost,
-                             NEW_HOST_PARAM: newHost,
+        sourceHost = '%s.%s.appspot.com' % (version, app)
+        targetHost = '%s.FIXME.appspot.com' % (version,)
+        content = CONTENT % {SOURCE_HOST_PARAM: sourceHost,
+                             TARGET_HOST_PARAM: targetHost,
                              TASK_NAME_PARAM: DEFAULT_TASK_NAME,
                              PATH: self.request.path_info}
         self.response.out.write(content)
     
     def post(self):
         """ see RequestHandler.post() """
-        assert OLD_HOST_PARAM in self.request.POST.keys()
-        assert NEW_HOST_PARAM in self.request.POST.keys()
+        assert SOURCE_HOST_PARAM in self.request.POST.keys()
+        assert TARGET_HOST_PARAM in self.request.POST.keys()
         
         # using one-time taskName ensure we don't start the migration twice
         taskName = self.request.POST.get(TASK_NAME_PARAM, DEFAULT_TASK_NAME)
@@ -81,9 +81,9 @@ class StartPullHandler(webapp.RequestHandler):
     """ Listening on the High-Replication application """
     def post(self):
         """ see RequestHandler.get() """
-        # using the old blob key in the task name ensures we don't pull the same blob multiple times
+        # using the source blob key in the task name ensures we don't pull the same blob multiple times
         taskName = self.request.POST.get(TASK_NAME_PARAM, DEFAULT_TASK_NAME)
-        taskName = taskName + '-' + self.request.POST[OLD_BLOB_KEY_PARAM].replace('=', '-') # '=' on dev_appserver
+        taskName = taskName + '-' + self.request.POST[SOURCE_BLOB_KEY_PARAM].replace('=', '-') # '=' on dev_appserver
         fantasm.startStateMachine(PULL_MACHINE_NAME, [self.request.POST], taskName=taskName, raiseIfTaskExists=False)
         
 def createApplication():
