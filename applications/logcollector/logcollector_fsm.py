e8bd5e02960dcede7724a33180aeafac5e6fd458
shawn.rusaw <shawn.rusaw@d05bc82c-ee18-d417-4187-6ce51f18356a>
2012-01-18 15:27:19 +0000

    - added an index page to make using logcollector easier

--- applications/logcollector/logcollector_fsm.py
+++ applications/logcollector/logcollector_fsm.py
@@ -1,10 +1,17 @@
 """ log collector
 
-"""
-import logging
-import time
+This application consists of a single state machine that runs as a continuation
+over App Engine logs fetched via the Log Reader API. The continuation token is
+the 'offset' parameter returned by logservice.fetch. When no logs are available,
+the fantasm infrastructure raises and re-tries the Task, backing off up to a 
+maximum of 60 seconds.
 
-import fantasm
+"""
+import time
+import pickle
+
+from fantasm.action import FSMAction
+from fantasm.action import ContinuationFSMAction
 
 from google.appengine.api import logservice
 from google.appengine.api import mail
@@ -17,15 +24,31 @@ VERSION_IDS_PARAM = 'version-ids'
 BATCH_SIZE_PARAM = 'batch-size'
 START_TIME_PARAM = 'start-time'
 
-EMAIL_PARAM = 'email'
+SENDER_EMAIL_PARAM = 'sender'
+TO_EMAIL_PARAM = 'to'
 
 OK = 'ok'
+
+# defaults different than logservice.fetch defaults
+DEFAULT_MINIMUM_LOG_LEVEL = logservice.LOG_LEVEL_CRITICAL
+DEFAULT_INCLUDE_APP_LOGS = True
+LOG_LEVEL_DESCRIPTION = {
+    logservice.LOG_LEVEL_DEBUG: 'logservice.LOG_LEVEL_DEBUG',
+    logservice.LOG_LEVEL_INFO: 'logservice.LOG_LEVEL_INFO',
+    logservice.LOG_LEVEL_WARNING: 'logservice.LOG_LEVEL_WARNING', 
+    logservice.LOG_LEVEL_ERROR: 'logservice.LOG_LEVEL_ERROR',
+    logservice.LOG_LEVEL_CRITICAL: 'logservice.LOG_LEVEL_CRITICAL'
+}
 
 class NoLogsAvailableException( Exception ):
     """ Exception raised when logservice.fetch returns nothing """
     pass
 
-class SetStartTime( fantasm.action.FSMAction ):
+class InvalidVersionException( Exception ):
+    """ Exception raised when trying to monitor self """
+    pass
+
+class SetStartTime( FSMAction ):
     """ Sets initial start_time """
     
     def execute(self, context, obj):
@@ -37,7 +60,7 @@ class SetStartTime( fantasm.action.FSMAction ):
         context[START_TIME_PARAM] = time.time()
         return OK
 
-class CollectLogs( fantasm.action.ContinuationFSMAction ):
+class CollectLogs( ContinuationFSMAction ):
     """ Collects AppEngine logs """
     
     def continuation(self, context, obj, token=None):
@@ -49,13 +72,13 @@ class CollectLogs( fantasm.action.ContinuationFSMAction ):
         if not token:
             offset = None
         else:
-            offset = token
+            offset = pickle.loads(str(token))
         
         startTime = context[START_TIME_PARAM]
         endTime = None
-        minimumLogLevel = context.get(MINIMUM_LOG_LEVEL_PARAM)
+        minimumLogLevel = context.get(MINIMUM_LOG_LEVEL_PARAM, DEFAULT_MINIMUM_LOG_LEVEL)
         includeIncomplete = context.get(INCLUDE_INCOMPLETE_PARAM, False)
-        includeAppLogs = context.get(INCLUDE_APP_LOGS_PARAM, True)
+        includeAppLogs = context.get(INCLUDE_APP_LOGS_PARAM, DEFAULT_INCLUDE_APP_LOGS)
         versionIds = context.get(VERSION_IDS_PARAM)
         if versionIds and not isinstance(versionIds, list):
             versionIds = [versionIds]
@@ -72,28 +95,38 @@ class CollectLogs( fantasm.action.ContinuationFSMAction ):
         
         logRequests = list(logRequestsIter)
         
+        # This effectively implements polling with exponential backoff, up to a
+        # maximum of 60s, as defined in fsm.yaml.
         if not logRequests:
             raise NoLogsAvailableException()
         
         obj[LOG_REQUESTS_PARAM] = logRequests
         
-        return logRequests[0].offset
-        
-    def execute(self, context, obj):
+        return pickle.dumps(logRequests[0].offset)
+    
+    def handleLogRequests(self, context, obj):
         """ Handles the logs. Currently just send an email to admin
         
         @param context: an FSMContext instance
         @param obj: a temporary state dictionary 
         """
-        
         logRequests = obj.get(LOG_REQUESTS_PARAM)
         if logRequests:
-            sender = to = context.get(EMAIL_PARAM)
-            if sender:
+            sender = context.get(SENDER_EMAIL_PARAM)
+            to = context.get(TO_EMAIL_PARAM)
+            if sender and to:
+                minimumLogLevel = context.get(MINIMUM_LOG_LEVEL_PARAM, DEFAULT_MINIMUM_LOG_LEVEL)
                 for logRequest in logRequests:
-                    subject = 'LogCollector detected messages matching threshold on PATH=' + logRequest.resource
+                    subject = 'LogCollector detected LEVEL=%s on PATH=%s' % \
+                                  (LOG_LEVEL_DESCRIPTION.get(minimumLogLevel, 'logservice.LOG_LEVEL_UNKNOWN'),
+                                   logRequest.resource)
                     body = '\n'.join([appLog.message for appLog in logRequest.app_logs]) or 'empty'
                     mail.send_mail(sender, to, subject, body)
         
+    def execute(self, context, obj):
+        """ Delegates to self.handleLogRequests
         
-
+        @param context: an FSMContext instance
+        @param obj: a temporary state dictionary 
+        """
+        return self.handleLogRequests(context, obj)
---
+++ applications/logcollector/main.py
@@ -0,0 +1,71 @@
+""" blobstore migration CGI script
+"""
+import os
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+
+from logcollector_fsm import VERSION_IDS_PARAM
+from logcollector_fsm import SENDER_EMAIL_PARAM
+from logcollector_fsm import TO_EMAIL_PARAM
+
+import fantasm
+
+LOG_COLLECTOR_MACHINE_NAME = 'LogCollector'
+
+CONTENT = """
+<html>
+<head>
+<title>Start LogCollector</title>
+</head>
+<body>
+<form method="POST">
+Version: <input name="version-ids" value="" size="25"/><br/>
+Minimum Log Level: <select name="minimum-log-level">
+  <option value="4">CRITICAL</option>
+  <option value="3">ERROR</option>
+  <option value="2">WARNING</option>
+  <option value="1">INFO</option>
+  <option value="0">DEBUG</option>
+</select><br/>
+Sender: <input name="sender" value="" size="25"/><br/>
+To: <input name="to" value="" size="25"/><br/>
+<input type="submit"/>
+</form>
+</body>
+</html>
+"""
+
+CURRENT_VERSION_ID = os.environ['CURRENT_VERSION_ID'].split('.')[0]
+            
+class StartLogCollectorHandler(webapp.RequestHandler):
+    """ Starts the LogCollector machine """
+    
+    def get(self):
+        """ see RequestHandler.get() """
+        self.response.out.write(CONTENT)
+    
+    def post(self):
+        """ see RequestHandler.post() """
+        assert VERSION_IDS_PARAM in self.request.POST.keys()
+        assert self.request.POST[VERSION_IDS_PARAM] != CURRENT_VERSION_ID, "Cannot monitor own version."
+        assert SENDER_EMAIL_PARAM in self.request.POST.keys(), "Require admin sender email address."
+        assert TO_EMAIL_PARAM in self.request.POST.keys(), "Require email address."
+        fantasm.startStateMachine(LOG_COLLECTOR_MACHINE_NAME, [self.request.POST])
+        self.response.out.write("LogCollector started on version '%s'." % self.request.POST[VERSION_IDS_PARAM])
+        
+def createApplication():
+    """ Returns a WSGI Application """
+    return webapp.WSGIApplication([
+        (r"^/logcollector/", StartLogCollectorHandler),
+    ],
+    debug=True)
+
+APP = createApplication()
+
+def main():
+    """ Main entry point. """
+    util.run_wsgi_app(APP)
+
+if __name__ == "__main__":
+    main()
