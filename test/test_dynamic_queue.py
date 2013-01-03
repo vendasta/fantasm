7ec9d0685f52e520717c25d6b8d429305669b190
Jason Collins <jcollins@vendasta.com>
2013-01-03 16:51:18 -0600

    - added a small functional test for context.setQueue()

---
+++ test/test_dynamic_queue.py
@@ -0,0 +1,32 @@
+"""
+Testing dynamic queue specificiation (context.setQueue())
+"""
+import os
+import logging
+
+from fantasm.action import FSMAction
+
+OK_EVENT = 'ok'
+
+def log_environ():
+    for key, value in os.environ.iteritems():
+        if not key.startswith('HTTP_X_APPENGINE_'):
+            continue
+        logging.info('%s: %s', key, value)
+
+class OnDefaultQueue(FSMAction):
+    def execute(self, context, obj):
+        log_environ()
+        context.setQueue('alternate-queue')
+        return OK_EVENT
+
+class OnDynamicQueue(FSMAction):
+    def execute(self, context, obj):
+        log_environ()
+        context.setQueue('default')
+        return OK_EVENT
+
+class BackOnDefaultQueue(FSMAction):
+    def execute(self, context, obj):
+        log_environ()
+        return None
